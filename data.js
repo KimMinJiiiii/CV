fb5bbeeeb4930fde944ff87abfb370a3bf0fd9bc
KimMinJiiiii <gkrtks40@naver.com>
2023-06-12 21:48:55 +0900

    Exp_2

--- data.js
+++ data.js
@@ -1,35 +1,45 @@
 const data = {
   experience: [
     {
-      post: "Software Development Engineer",
-      company: "PuStack",
-      link: "https://pustack.com/",
-      dates: "July 2020 - Present",
-      span: "1 Month",
-      stack: "Flutter, Dart, UI/UX",
-      description: `Under this role, I will be responsible for provisioning scalable infrastructure enabling both the business and its customers through the means of cutting edge technological products. My primary tech stack for this role would be Flutter.`,
+      post: " Writing Challengr Project ",
+      // company: "PuStack",
+      link: "https://github.com/edw11/Writing_6-8.git",
+      // dates: "July 2020 - Present",
+      //span: "1 Month",
+      // stack: "Flutter, Dart, UI/UX",
+      description: `이 프로젝트는 꾸준히 일기를 쓰기 어려워 하거나 창의성이 부족한 사람들을 위해 진행한 프로젝트입니다.
+      
+                  우리의 프로그램을 통해서 많은 사람들이 글을 쓰는 것을 두려워 하지 않고 일기를 써가며 자신의 이야기를
+                  잘 풀어 낼 수 있는 효과를 기대합니다.`,
     },
     {
-      post: "Technical Author",
-      company: "Cypher Eruption",
-      link: "https://cyphereruption.net",
-      dates: "August 2014 - Sept 2016",
-      span: "2 Years",
-      stack: "VB.Net, Visual C#, Wordpress",
-      description: `Cypher Eruption is a free place to learn best programming languages and ethical hacking. Be a learner and learn programming and ethical hacking or be a teacher and teach anything unique. We respect every single visitor, author, contributor. Because of you we are here.`,
+      post: "책 추천 프로그램",
+      //company: "Cypher Eruption",
+      //link: "https://cyphereruption.net",
+      //dates: "August 2014 - Sept 2016",
+      //span: "2 Years",
+      //stack: "VB.Net, Visual C#, Wordpress",
+      description: ` 이 프로젝트를 진행하면서 요즘 책 읽는 사람들이 저조한 상황에서의 이유를 찾고자 했을 때
+
+      책을 읽는 것이 어렵거나 다른 장르를 읽어보고 싶지만 어려움이 있어 쉽게 도전할 수 없는 사람들 등
+      다양한 이유를 통합하여 해결할 수 있는 프로그램을 만들면 좋을 것 같아서 시작하게 되었습니다. 
+      
+      '책 읽기 추천 프로그램'을 통해서 많은 사람들이 다양한 책을 접하며 스스로의 역량을 키워나아갔으면 합니다.`,
     },
     {
-      post: "Frontend Developer",
-      company: "Developer Students Clubs, Chitkara University",
-      link: "#",
-      dates: "October 2019 - Present",
-      span: "8 Months",
-      stack: "HTML5, CSS, JavaScript, jQuery, Flutter",
+      //post: "Frontend Developer",
+      //company: "Developer Students Clubs, Chitkara University",
+      //link: "#",
+      //dates: "October 2019 - Present",
+      //span: "8 Months",
+      //stack: "HTML5, CSS, JavaScript, jQuery, Flutter",
       description: `Developer Student Clubs are university based community groups for students interested in Google developer technologies. Students from all undergraduate or graduate programs with an interest in growing as a developer are welcome. By joining a DSC, students grow their knowledge in a peer-to-peer learning environment and build solutions for local businesses and their community.
 
 `,
     },
+    
   ],
+  
   achievements: [
     {
       title: "3rd Rank",
